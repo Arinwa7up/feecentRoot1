8ca2f419cda2ce151abdb67c743e71a53b864a8e
Arinwa7up <eluagu18@gmail.com>
2026-07-11 15:15:26 -0700

    Rewrite full project files

--- api/cron/process-savings.js
+++ api/cron/process-savings.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   /*const authHeader = req.headers.authorization;
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).json({ error: "Unauthorized" });
-  }*/
+  }  */
 
   try {
     await processAllSavings();
